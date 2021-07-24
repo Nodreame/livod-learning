cff9f621b4e9a0756f19f85950833a9ee2dd20b4
Nodreame <nodreame94@gmail.com>
2021-07-24 23:17:49 +0800

    feat: 配置 Markdown 数据转换插件 & 完成文章列表开发

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,55 @@
+import { graphql } from 'gatsby'
 import React from 'react'
 import Layout from '../component/layout'
 
-const Home = () => (
+export const query = graphql`
+  query {
+    allMarkdownRemark(filter: {}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            author
+            date
+            tag
+            title
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`
+
+const Home = ({ data }) => (
   <Layout>
-    <div>Hello livod!</div>
+    <h1>文章列表</h1>
+    {data.allMarkdownRemark.edges.map(({ node }) => (
+      <div key={node.id}>
+        <h3>
+          {node.frontmatter.title}
+          {node.frontmatter.tag && (
+            <span
+              style={{
+                padding: '5px',
+                fontSize: '14px',
+                color: '#fff',
+                background: 'rgba(26, 189, 94, 0.8)',
+                borderRadius: '2px',
+                marginLeft: '5px',
+              }}
+            >
+              {node.frontmatter.tag}
+            </span>
+          )}
+        </h3>
+        <p style={{ color: '#aaa' }}>
+          {node.frontmatter.date && <span style={{ marginRight: '10px' }}>{node.frontmatter.date}</span>}
+          {node.frontmatter.author && <span>{node.frontmatter.author}</span>}
+        </p>
+        <p>{node.excerpt}</p>
+      </div>
+    ))}
   </Layout>
 )
 
